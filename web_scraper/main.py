9d5b7bff955c313370d4925d8ab73c1236d14b48
zassou-robo <zassou.sub@gmail.com>
2026-07-06 16:22:32 +0900

    Atlus公式サイトのHTMLを取得し保存

--- web_scraper/main.py
+++ web_scraper/main.py
@@ -1,8 +1,15 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = "https://www.atlus.co.jp/"
 
 response = requests.get(url)
-
-print(response.status_code)
-print(response.text[:500])
+response.encoding = response.apparent_encoding
+soup = BeautifulSoup(response.text, "lxml")
+# with open("atlus.html", "w", encoding="UTF-8") as f:
+#     f.write(soup.prettify())
+news_list = soup.find_all("li", class_="news-item")
+print(len(news_list))
+print("HTML saved to atlus.html")
+# for news in news_list:
+#     print(news.get_text())
